82e278ca346d8bcb0ae0f530c8ab5d959afadc3a
Mohamed Hamdy <98131964+Adosh74@users.noreply.github.com>
2023-06-06 02:42:19 +0300

    all

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+/* eslint-disable no-undef */
+module.exports = {
+	env: {
+		browser: true,
+		es2021: true,
+		node: true,
+	},
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'prettier',
+	],
+	overrides: [],
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+	},
+	plugins: ['@typescript-eslint', 'prettier'],
+	rules: {
+		'prettier/prettier': ['error', { endOfLine: 'auto' }],
+		'no-console': 0,
+		'no-var': 2,
+		'prefer-const': 'off',
+	},
+};
