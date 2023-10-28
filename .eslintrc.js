7f491a6100f689318c978a73941c2d3fdadd8135
Mohamed Hamdy <98131964+Adosh74@users.noreply.github.com>
2023-10-28 19:02:24 +0200

    udate

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,25 @@
-/* eslint-disable no-undef */
+/* eslint-disable */
 module.exports = {
 	env: {
 		browser: true,
 		es2021: true,
-		node: true,
 	},
-	extends: [
-		'eslint:recommended',
-		'plugin:@typescript-eslint/recommended',
-		'prettier',
-	],
+	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
 	overrides: [],
 	parser: '@typescript-eslint/parser',
 	parserOptions: {
 		ecmaVersion: 'latest',
 		sourceType: 'module',
 	},
+
 	plugins: ['@typescript-eslint', 'prettier'],
 	rules: {
-		'prettier/prettier': ['error', { endOfLine: 'auto' }],
-		'no-console': 0,
-		'no-var': 2,
-		'prefer-const': 'off',
+		'prettier/prettier': [
+			'warn',
+			{ endOfLine: 'auto', singleQuote: true, parser: 'flow' },
+		],
+		'no-var': 'error',
+		'@typescript-eslint/no-explicit-any': 'off',
+		'@typescript-eslint/no-unused-vars': 'warn',
 	},
 };
--- src/server.ts
+++ src/server.ts
@@ -2,5 +2,6 @@ import express from 'express';
 
 const app = express();
 
-app.listen(3001);
-
+app.listen(3001, () => {
+	console.log('Server started on port 3001');
+});
